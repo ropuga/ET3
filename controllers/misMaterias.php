2c4508f329c21961eaf4fe0456a7b41a1aa12559
Daniel <ddduran@esei.uvigo.es>
2015-12-27 18:48:27 +0100

    Mis MAterias

---
+++ controllers/misMaterias.php
@@ -0,0 +1,34 @@
+<?php
+  // Plantilla de creación de un controlador por Martín Vázquez
+
+  session_start(); // se inicia el manejo de sesiones
+
+  //Includes iniciales
+  require_once '../views/templateEngine.php'; // se carga la clase TemplateEngine
+  require_once '../model/Materia.php';
+  require_once '../model/Usuario.php';
+  require_once '../model/driver.php';
+  require_once 'navbar.php'; //Inclusión de navbar. Omitible si no la necesita
+
+
+  //Conexion a la BD
+  $db = Driver::getInstance();
+
+  //Instancias TemplateEngine, renderizan elementos
+  $renderMain = new TemplateEngine();
+  $rendermistit = new TemplateEngine();
+  $titulaciones = new Titulacion($db);
+  $usuario = new Usuario($db);
+  $usuario = $usuario->findBy('user_name',$_SESSION['name']);
+  $misMaterias = $usuario[0]->materias();
+  $allMaterias = $materias->all();
+
+  $rendermistit->misMaterias = $misMaterias;
+  $rendermistit->allMaterias= $allMaterias;
+  $renderMain->title = "Mis materias"; //Titulo y cabecera de la pagina
+  $renderMain->navbar = renderNavBar(); //Inserción de navBar en la pagina. Omitible si no la necesita
+  $renderMain->content = $rendermistit->render('misMaterias_v.php'); //Inserción del contenido de la página
+
+  echo $renderMain->renderMain(); // Dibujado de la página al completo
+
+ ?>
--- model/Usuario.php
+++ model/Usuario.php
@@ -127,6 +127,17 @@ class Usuario{
     $results = $this->driver->exec($query);
     return $titulacion->factory($results);
   }
+
+  public function materias(){
+    $materia = Materia($this->driver);
+    $query = "select * from Usuario,Materia,Materia_Usuario where
+              Usuario.user_id = MAteria_Usuario.user_id and
+              Materia_Usuario.mat_id = Materia.mat_id and
+              Usuario.user_id = '".$this->user_id."'";
+    $results = $this->driver->exec($query);
+    return $materia->factory($results);
+  }
+
   public function tieneApuntes(){
     $apunte = new Apunte($this->driver);
     $query = "select Apunte.apunte_id,Apunte.mat_id,Apunte.anho_academico,Apunte.apunte_name,Apunte.ruta, Apunte.user_id from Usuario,Apunte,U_Tiene_A where
