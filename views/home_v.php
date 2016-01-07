a840040d12d5ffa94b079e51be462650bc7cc6ad
colmilloestelar <rvramos@hotmail.es>
2016-01-07 20:54:33 +0100

    home
    easy nobs

--- views/home_v.php
+++ views/home_v.php
@@ -1,4 +1,20 @@
 <div> PÁGINA PRINCIPAL</div>
-<div>Tabla apuntes recientes</div>
-<div class="bgigante btn btn-success ">boton subir apuntes</div>
-<div class="bgigante btn btn-success">boton nueva nota</div>
+<div>
+  <p>Tabla apuntes recientes</p>
+  <br>
+  <?php foreach($apuntes as $a): ?>
+      <span class="izquierda">
+      <span class="materia"><?php echo $a->getApunte_name(); ?></span>&nbsp;&nbsp;&nbsp;</span>
+      <span class="materia"><?php echo $a->nombreTitulacion(); ?></span>&nbsp;&nbsp;&nbsp;</span>
+      <span class="materia"><?php echo $a->nombreMateria(); ?></span>&nbsp;&nbsp;&nbsp;</span>
+      <span class="materia"><?php echo $a->getAnho_academico(); ?></span>&nbsp;&nbsp;&nbsp;</span>
+      <span class="materia"><?php echo $a->nombreAutor(); ?></span>&nbsp;&nbsp;&nbsp;</span>
+  <?php endforeach; ?>
+</div>
+
+<div>
+  <a  href="../controllers/subirApunte.php" class="bgigante btn btn-success"> Subir un apunte </a>
+</div>
+<div>
+   <a href="nuevaNota.php" class="bgigante btn btn-success">Crear Nueva Nota</a>
+</div>
