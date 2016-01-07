dd3371dc762a46891cc025185ef1e0d0fa2af8f5
colmilloestelar <rvramos@hotmail.es>
2016-01-07 18:51:08 +0100

    hom

--- views/home_v.php
+++ views/home_v.php
@@ -1,4 +1,4 @@
 <div> PÁGINA PRINCIPAL</div>
 <div>Tabla apuntes recientes</div>
-<div>boton subir apuntes</div>
-<div>boton nueva nota</div>
+<div class="bgigante btn btn-success ">boton subir apuntes</div>
+<div class="bgigante btn btn-success">boton nueva nota</div>
